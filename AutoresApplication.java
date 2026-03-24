fa1d4ce23c435e929a209654d1ac1bd28f865c72
JoaoGMB <muller.bordin@gmail.com>
2026-03-23 21:54:21 -0300

    Add files via upload

---
+++ AutoresApplication.java
@@ -0,0 +1,11 @@
+package com.example.autores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoresApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AutoresApplication.class, args);
+    }
+}
